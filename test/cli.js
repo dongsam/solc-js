b6543664c52e466784323e441b5e34ea4aa463dc
Alex Beregszaszi <alex@rtfs.hu>
2017-07-25 21:04:13 +0100

    Wait until stdin piping finished

--- test/cli.js
+++ test/cli.js
@@ -61,10 +61,12 @@ tape('CLI', function (t) {
     spt.stdin.setEncoding('utf-8');
     spt.stdin.write(JSON.stringify(input));
     spt.stdin.end();
-    spt.stderr.empty();
-    spt.stdout.match(/Contract.sol/);
-    spt.stdout.match(/userdoc/);
-    spt.succeeds();
-    spt.end();
+    spt.stdin.on('finish', function () {
+      spt.stderr.empty();
+      spt.stdout.match(/Contract.sol/);
+      spt.stdout.match(/userdoc/);
+      spt.succeeds();
+      spt.end();
+    });
   });
 });
